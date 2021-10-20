5ea4fcdfab5d8763551b4ae3e61f2c96bd8649a8
vanhopld98 <vanhopld98@gmail.com>
2021-10-20 10:39:59 +0700

    first commit

---
+++ src/main/java/com/codegym/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+    @GetMapping
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("user", new User());
+        return modelAndView;
+    }
+
+    @PostMapping("/validateUser")
+    public ModelAndView checkValidate(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            return new ModelAndView("/index");
+        }
+        return new ModelAndView("/result");
+    }
+}
